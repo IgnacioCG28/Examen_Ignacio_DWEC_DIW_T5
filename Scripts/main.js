05fc56fc91d2940d6b10d78dc1bb377b14586440
IgnacioCG28 <ignaciocarmonagonzalez@alumnos.tuniversformacion.es>
2024-01-17 10:01:17 +0100

    css casi terminado, inicio css

---
+++ Scripts/main.js
@@ -0,0 +1,20 @@
+/**
+ * @author Nacho Carmona Gonzalez;
+ * @github ????
+ * @version 0.0
+ * @description: ExamenT5
+ */
+
+const formulario = document.querySelector("#formulario");
+const nombre = document.querySelector("#nombre");
+const correo = document.querySelector("#correo");
+const mensaje = document.querySelector("#mensaje");
+const enviar = document.querySelector("#enviar");
+const errores = document.querySelector("#errores");
+const radioInfo = document.querySelector("radioInfo")
+const radioSend = document.querySelector("radioSend")
+let mensajesErrores = [];
+
+
+
+  
