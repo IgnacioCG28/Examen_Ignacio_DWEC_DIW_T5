c7b7155207bdcc4dea35795c710fb50c5a67e4fa
IgnacioCG28 <ignaciocarmonagonzalez@alumnos.tuniversformacion.es>
2024-01-17 10:12:58 +0100

    ajuste radio

--- Scripts/main.js
+++ Scripts/main.js
@@ -8,11 +8,12 @@
 const formulario = document.querySelector("#formulario");
 const nombre = document.querySelector("#nombre");
 const correo = document.querySelector("#correo");
+const radioInfo = document.querySelector("radioInfo");
+const radioSend = document.querySelector("radioSend");
 const mensaje = document.querySelector("#mensaje");
 const enviar = document.querySelector("#enviar");
 const errores = document.querySelector("#errores");
-const radioInfo = document.querySelector("radioInfo")
-const radioSend = document.querySelector("radioSend")
+
 let mensajesErrores = [];
 
 
@@ -22,8 +23,10 @@ const validar = (event) => {
     mensajesErrores = [];
       //Nombre
     nombre.value.trim().length === 0 && mensajesErrores.push("El nombre es un campo obligatorio");
-    nombre.value !== 
+    
     }
   
   
   formulario.addEventListener("submit", validar);
+
+  
